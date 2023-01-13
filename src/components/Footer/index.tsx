60f16e6ced94f3b1d353c3d818831e38b6205415
amine00012 <aminebarka1212@gmail.com>
2023-01-13 09:15:25 +0100

    fix(ui): fix `display` in  index component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React from 'react';
 
+import 'animate.css';
 import "./index.css";
 
 export function Footer() {
 
-    return (
-			<footer
-				className="my-selection-footer global-footer__clz"
-				style={{ marginTop: "auto", backgroundColor: "rgb(253, 253, 253)" }}
-			>
-				<div className="divider__clz">
-					<hr></hr>
-				</div>
-				<div className="footer-text__clz justify-content-start">
-					<div className="mt-4 mx-1 mb-md-4 px-4 text-footer__clz ">
-						saber® {new Date().getFullYear()}
-					</div>
-				</div>
-			</footer>
-		);
+	return (
+		<footer className="footer-bs">
+        <div className="row">
+		<div className="col-md-3 footer-brand animated fadeInLeft">
+		<img src="logo.pnj" alt=""/>
+                 <p>2023 ©  BS3 UI Kit, All rights reserved</p>
+            </div>
+        <div className="col-md-4 footer-nav animated fadeInUp">
+            <h4>Menu</h4>
+            <div className="col-md-6">
+                <ul className="pages">
+                <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contacts</a></li>
+                    <li><a href="#">Terms & Condition</a></li>
+                    <li><a href="#">Privacy Policy </a></li>
+                </ul>
+            </div>
+			</div>
+            <div className="col-md-2 footer-social animated fadeInDown">
+                <h4>Follow Us</h4>
+                <ul>
+                    <li><a href="#">Facebook</a></li>
+                    <li><a href="#">Twitter</a></li>
+                    <li><a href="#">Instagram</a></li>
+                    <li><a href="#">Github</a></li>
+					</ul>
+            </div>
+            <div className="col-md-3 footer-ns animated fadeInRight">
+                <h4>Newsssdvdsdvvvlettdver</h4>
+                <p>A vsdvrvdvsdvver vdswesdring a sdvfuzzsdvy suidvt ddvoesn’t slarm thvvssde real pensdvuvdsins</p>
+                <p>
+                    <div className="input-group">
+                        <input type="text" className="form-control" placeholder="Search for..."/>
+                        <span className="input-group-btn">
+                            <button className="btn btn-default" type="button">
+                                <span className="glyphicon glyphicon-envelope"></span>
+                            </button>
+                        </span>
+                    </div>
+                </p>
+            </div>
+        
+		</div>
+		</footer>
+    );
 }
