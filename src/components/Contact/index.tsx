e3403869a981ece6c995d14b5340c2479e90a416
Dalel Akremi <dalelakremi2020@gmail.com>
2023-01-13 10:13:16 +0100

    feat: add `contact` component

---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,49 @@
+import './index.css';
+
+export default function  Contact(){
+	return <div className="contact_container">
+			<div className="contact_form_container">
+				<div className="left">
+					<h1>Welcome</h1>
+						<button type="button" className="white_btn">
+						contact us
+						</button>
+		
+				</div>
+				<div className="right">
+					<form className="form_container"  >
+						<h1>contact</h1>
+						<input
+							type="text"
+							placeholder="First Name"
+							name="firstName"
+							className="input"
+						/>
+						<input
+							type="text"
+							placeholder="Last Name"
+							name="lastName"
+							className="input"
+						/>
+						<input
+							type="email"
+							placeholder="Email"
+							name="email"
+							className="input"
+						/>
+						<input
+							type="password"
+							placeholder="Password"
+							name="password"
+							className="input"
+						/>
+		
+						<button type="submit" className="green_btn">
+							ok
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+
+};
