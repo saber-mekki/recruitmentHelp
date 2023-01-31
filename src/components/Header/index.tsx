2884014576371d1a93ebcd35419b06e9d54e8756
amine00012 <aminebarka1212@gmail.com>
2023-01-31 12:17:08 +0100

    feat (Header): add a new style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,83 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from "../common/Container";
+import { SvgIcon } from "../common/SvgIcon";
+import { Button } from "../common/Button";
 import logo from './aures.png';
 
-import "./index.css";
-export function Header() {
-    return (
-			<div className="header-container__clz">
-				<img src={logo} alt="logo" className="img" />
-				<div className="container-fluid">
-					<div className="row">
-						<div className="col-xl-3 col-lg-3 col-md-3 col-sm-3 col logo_section">
-							<div className="full"></div>
-						</div>
-					</div>
-				</div>
-				<h5 className="text-header-container__clz">AURES Recrutment</h5>
+import {
+	HeaderSection,
+	LogoContainer,
+	Burger,
+	NotHidden,
+	Menu,
+	CustomNavLinkSmall,
+	Label,
+	Outline,
+	Span,
+  } from "./styles";
 
-				<div className="col-xl-9 col-lg-9 col-md-9 col-sm-9">
-					<nav className="navigation navbar navbar-expand-md navbar-dark ">
-						<button
-							className="navbar-toggler"
-							type="button"
-							data-toggle="collapse"
-							data-target="#navbarsExample04"
-							aria-controls="navbarsExample04"
-							aria-expanded="false"
-							aria-label="Toggle navigation"
-						>
-							<span className="navbar-toggler-icon"></span>
-						</button>
+import { Row, Drawer, Col } from "antd";
 
-						<div className="collapse navbar-collapse" id="navbarsExample04">
-							<ul className="navbar-nav mr-auto">
-								<li className="nav-item active">
-									<a className="nav-link" href="index">
-										Home
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="about">
-										About Us
-									</a>
-								</li>
 
-								<li className="nav-item">
-									<a className="nav-link" href="product">
-										Products
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="roles">
-										roles
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="candidates">
-									Candidates
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="entretiens">
-									Entretiens
-									</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="contact">
-										Contact Us
-									</a>
-								</li>
-								<li className="nav-item d_none">
-									<a className="nav-link" href="search">
-										<i className="fa fa-search" aria-hidden="true"></i>
-									</a>
-								</li>
-								
-							</ul>
-						</div>
-					</nav>
-				</div>
-			</div>
-		);
-}
-export default Header ;
+export function Header ()  {
+	const [visible, setVisibility] = useState(false);
+  
+	const showDrawer = () => {
+	  setVisibility(!visible);
+	};
+  
+	const onClose = () => {
+	  setVisibility(!visible);
+	};
+  
+	const MenuItem = () => {
+	  const scrollTo = (id: string) => {
+		const element = document.getElementById(id) as HTMLDivElement;
+		element.scrollIntoView({
+		  behavior: "smooth",
+		});
+		setVisibility(false);
+	  };
+	  
+	  return (
+		<>
+		  <CustomNavLinkSmall>
+			<Span><a href="About">{("About")}</a></Span>
+		  </CustomNavLinkSmall>
+		  <CustomNavLinkSmall>
+			<Span><a href="Roles">{("Roles")}</a></Span>
+		  </CustomNavLinkSmall>
+		  <CustomNavLinkSmall>
+			<Span><a href="entretiens">{("entretiens")}</a></Span>
+		  </CustomNavLinkSmall>
+		  <CustomNavLinkSmall>
+			<Span><a href="condidates">{("condidates")}</a></Span>
+		  </CustomNavLinkSmall>
+		  <CustomNavLinkSmall
+			style={{ width: "180px" }}
+			
+		  >
+			<Span>
+			<a href="contact">
+			
+			  <Button>{("Contact")}</Button>
+			  </a>
+			</Span>
+		  </CustomNavLinkSmall>
+		</>
+	  );
+	};
+  
+	return (
+	  <HeaderSection>
+		<Container>
+		  <Row justify="space-between">
+			<LogoContainer to="/" aria-label="homepage">
+			  <SvgIcon src={logo} width="101px" height="64px" />
+			</LogoContainer>
+			<NotHidden>
+			  <MenuItem />
+			</NotHidden>
+			<Burger onClick={showDrawer}>
+			  <Outline />
+			</Burger>
+		  </Row>
+		  <Drawer closable={false} visible={visible} onClose={onClose}>
+			<Col style={{ marginBottom: "2.5rem" }}>
+			  <Label onClick={onClose}>
+				<Col span={10}>
+				  <Menu>Menu</Menu>
+				</Col>
+				<Col span={10}>
+				  <Outline />
+				</Col>
+			  </Label>
+			</Col>
+			<MenuItem />
+		  </Drawer>
+		</Container>
+	  </HeaderSection>
+	);
+  };
+  
